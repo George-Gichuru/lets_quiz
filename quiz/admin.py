81a468940b0e5f21b3616fc13837146804940e22
GEORGE-GICHURU <gichurugeorge092@gmail.com>
2022-11-06 16:52:49 +0300

    registering the

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question, Choice
+from .forms import QuestionForm, ChoiceForm, ChoiceInlineFormset
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    can_delete = False
+    max_num = Choice.MAX_CHOICES_COUNT
+    min_num = Choice.MAX_CHOICES_COUNT
+    form = ChoiceForm
+    formset = ChoiceInlineFormset
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    model = Question
+    inlines = (ChoiceInline, )
+    list_display = ['html', 'is_published']
+    list_filter = ['is_published']
+    search_fields = ['html', 'choices__html']
+    actions = None
+    form = QuestionForm
+
+
+admin.site.register(Question, QuestionAdmin)
