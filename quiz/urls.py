3b4602b6408b478e395d45c87bc86fe58df81e1f
GEORGE-GICHURU <gichurugeorge092@gmail.com>
2022-11-06 18:52:51 +0300

    urls.py

---
+++ quiz/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+
+app_name = 'quiz'
+
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('user-home/', views.user_home, name='user_home'),
+    path('play/', views.play, name='play'),
+    path('leaderboard/', views.leaderboard, name='leaderboard'),
+    path('submission-result/(?P<attempted_question_pk>/)',
+         views.submission_result, name='submission_result'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.login_view, name='logout'),
+    path('register/', views.register, name='register'),
+
+]
