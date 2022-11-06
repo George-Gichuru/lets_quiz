2747f2b17ee4d257935792006a90a8016bf1d1b8
GEORGE-GICHURU <gichurugeorge092@gmail.com>
2022-11-06 16:27:10 +0300

    templates section

--- lets_quiz/settings.py
+++ lets_quiz/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'quiz',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -116,6 +118,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_URL = '/login'
+LOGIN_REDIRECT_URL = '/user_home'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
